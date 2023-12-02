c49efcdc9df93c492adad1cb3a49e43d95803171
Simon Dürr <dev@simonduerr.eu>
2023-12-02 20:21:49 +0100

    add loupedeck listener

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,103 @@
 <script setup>
+
 import MolecularViewer from "./components/MolecularViewer.vue";
 import ColorPicker from "./components/ColorPicker.vue";
 import LoupedeckListener from "./components/LoupedeckListener.vue";
+
+import PDBInput from './components/PDBInput.vue';
+import { ref } from 'vue';
+
+
+async function getInChainsValues(entryId) {
+  const apiUrl = `https://www.ebi.ac.uk/pdbe/api/pdb/entry/molecules/${entryId}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data[entryId]) {
+      const molecules = data[entryId];
+      const inChainsValues = molecules.map(molecule => molecule.in_chains).flat();
+      return Array.from(new Set(inChainsValues));
+    } else {
+      throw new Error(`Entry ${entryId} not found`);
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    return null;
+  }
+}
+
+
+
+let entryId = ref("1pga")
+let chains = ref([])
+
+let chainColors = ref({})
+
+let showViewer = ref(false)
+
+let currentTarget = ref("*")
+
+
+async function loadpdb(pdbcode) {
+
+  entryId.value = pdbcode
+  chains.value = await getInChainsValues(pdbcode)
+
+  // insert chain * at beginning of chain list
+  chains.value = ["*"].concat(chains.value)
+
+
+
+  //foreach chain insert hsl object
+  chains.value.forEach(chain => {
+    chainColors.value[chain] = { h: 0, s: 0, l: 0 }
+  })
+  showViewer.value = true
+}
+
+function updateColor(color) {
+  alert("update color")
+  chainColors.value[currentTarget] = color
+}
+
+function updateTarget(target) {
+  alert("update target")
+  currentTarget.value = target
+}
+
+
 </script>
 
 <template>
   <div>
     <h1 class="text-xl font-bold">PDB2Vector</h1>
-    <LoupedeckListener />
-    <div class="flex">
-      <div class="w-1/2">
-        <ColorPicker />
+
+    <LoupedeckListener @color-changed="updateColor" @chain-changed="updateTarget" />
+
+    <!-- ToDO make component look nice, display loading -->
+    <PDBInput @load="loadpdb" />
+
+    <code>
+                         {{ JSON.stringify(chainColors) }}
+                         {{ JSON.stringify(currentTarget) }}
+                        </code>
+    <!-- animate transition -->
+    <transition>
+      <div v-if="showViewer">
+        <div class="flex">
+          <div class="w-1/2">
+            <ColorPicker @colorchanged="updateColor" @chainchanged="updateTarget" />
+          </div>
+          <div class="w-1/2">
+            <MolecularViewer :pdb="entryId" :colors="chainColors" />
+          </div>
+        </div>
+
       </div>
-      <div class="w-1/2">
-        <MolecularViewer />
-      </div>
-    </div>
+    </transition>
+
   </div>
 </template>
 
---
+++ src/components/PDBInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input type="text" placeholder="PDB Code" v-model="pdb">
+        <button @click="load">Fetch</button>
+    </div>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+
+const pdb = ref('1pga')
+const emit = defineEmits(['load'])
+// emit load event with pdb code if pdb code is valid
+function load() {
+    if (pdb.value.length == 4) {
+        emit('load', pdb.value)
+    }
+}
+</script>
+
+<style></style>
