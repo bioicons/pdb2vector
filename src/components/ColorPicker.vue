c6cee7fc2c3512571a011a6e2fbc47e48a180d9d
rush42 <laurenz.rasche@gmail.com>
2023-12-03 00:03:38 +0100

    guard for null

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -3,9 +3,9 @@
     <label for="tabs" class="sr-only">Chain</label>
     <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
     <div class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500">
-      <button v-for="tab in tabs" :key="tab"
-        :class="[[tab == currentChain ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700', 'rounded-md px-3 py-2 text-sm font-medium']]"
-        @click="setcurrentChain(tab)">{{ tab }}</button>
+      <button v-for="chain in Object.keys(chains)" :key="chain"
+        :class="[[chain == currentChain ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700', 'rounded-md px-3 py-2 text-sm font-medium']]"
+        @click="setCurrentChain(chain)">{{ chain }}</button>
     </div>
   </div>
   <button as="div" :class="[currentChain == '*' && 'hidden', 'flex items-center']" @click="enableCustomColor(currentChain)">
@@ -31,7 +31,7 @@
         </RadioGroupOption>
       </div>
     </RadioGroup>
-    <color-picker variant='persistent' :hue="hue" @input="updateColor" />
+    <color-picker variant='persistent' :hue="colorHSL[0]" :saturation="colorHSL[1]" :luminosity="colorHSL[2]" @input="updateColor" saturation="0" />
   </div>
 </template>
 
@@ -47,9 +47,9 @@ const currentChain = ref("*")
 
 const defaultColors = [[256, 0, 256], [0, 256, 256], [0, 0, 256], [0, 256, 0], [0, 0, 0], [256, 256, 256], [0, 128, 256], [256, 0, 128], [128, 0, 256]]
 const colors = reactive([chains["*"], ...defaultColors])
-const hue = computed(() => RGB2HSL(...chains[currentChain.value] )[0]) 
+const colorHSL = computed(() => RGB2HSL(...(chains[currentChain.value] || [0,0,0])))
 
-const setcurrentChain = (val) => {
+const setCurrentChain = (val) => {
   currentChain.value = val
 }
 const enableCustomColor = (chain) => {
