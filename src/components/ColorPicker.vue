061a1ec18bcb67e7d668f6ecadb53c691a368c50
rush42 <laurenz.rasche@gmail.com>
2023-12-02 17:56:53 +0100

    store colors as rgb values and generate random colors

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,30 +1,42 @@
 <template>
+  <div class="w-60">
   <RadioGroup v-model="selectedColor">
     <RadioGroupLabel class="block text-sm font-medium leading-6 text-gray-900">Choose a label color</RadioGroupLabel>
-    <div class="mt-4 flex items-center space-x-3">
-      <RadioGroupOption as="template" v-for="color in colors" :key="color.name" :value="color" v-slot="{ active, checked }">
-        <div :class="[color.selectedColor, active && checked ? 'ring ring-offset-1' : '', !active && checked ? 'ring-2' : '', 'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none']">
-          <RadioGroupLabel as="span" class="sr-only">{{ color.name }}</RadioGroupLabel>
-          <span aria-hidden="true" :class="[color.bgColor, 'h-8 w-8 rounded-full border border-black border-opacity-10']" />
+    <div class="grid grid-cols-5 gap-4">
+      <RadioGroupOption as="template" v-for="color in colors" :key="color" :value="color" v-slot="{ active, checked }">
+        <div :class="[active && checked ? ' ring ring-offset-1' : '', !active && checked ? 'ring-2' : '', 'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-gray-700']">
+          <span aria-hidden="true" class='h-8 w-8 rounded-full border border-black border-opacity-10' :style="{'background-color': `rgb(${color.join(space)})`}"/>
         </div>
       </RadioGroupOption>
     </div>
   </RadioGroup>
+  <div class="relative flex cursor-pointer items-center justify-center rounded-full p-2">
+    <button aria-hidden="true" class="h-8 w-8 rounded-full border border-black border-opacity-10" @click="addCustomColor()"/>
+  </div>
+</div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue'
 
-const colors = [
+const defaultColors = [[100, 0, 0], [100, 0, 100], [0, 100, 100], [0, 0, 100], [0, 100, 0]]
+const space = " "
 
-
-  { name: 'Pink', bgColor: 'bg-pink-500', selectedColor: 'ring-pink-500' },
-  { name: 'Purple', bgColor: 'bg-purple-500', selectedColor: 'ring-purple-500' },
-  { name: 'Blue', bgColor: 'bg-blue-500', selectedColor: 'ring-blue-500' },
-  { name: 'Green', bgColor: 'bg-green-500', selectedColor: 'ring-green-500' },
-  { name: 'Yellow', bgColor: 'bg-yellow-500', selectedColor: 'ring-yellow-500' },
+const customColors = [
 ]
 
-const selectedColor = ref(colors[1])
+const addCustomColor = () => {
+  const color = [Math.round(Math.random() * 256), Math.round(Math.random() * 256), Math.round(Math.random() * 256)] 
+    if (customColors.length > 4) {
+      customColors.shift()
+    }    
+    customColors.push(color)
+    colors.length = 0
+    colors.push(...defaultColors)
+    colors.push(...customColors)
+}
+
+const colors = reactive([...defaultColors, ...customColors])
+const selectedColor = ref(defaultColors[1])
 </script>
